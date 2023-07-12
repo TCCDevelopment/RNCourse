3ad409453875f199cfe774e8775fbe7b0c85ae8f
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-12 16:46:47 -0700

    Lesson 18-21 Flexbox
    19: Playing with Flexbox to make basic layout with the stylesheet

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your course goal!" />
-        <Button title="Add Goal"/>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal" />
       </View>
       <View>
         <Text>List of goals....</Text>
@@ -17,5 +17,16 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
-  }
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight:8,
+    padding:8
+  },
 });
