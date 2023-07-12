e31c48a977ff900d19d18aff4d46bc62a43eefa9
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-12 16:18:59 -0700

    Lesson 17 - Starting from scratch
    Deleting previous components and starting to build an app from scratch

--- App.js
+++ App.js
@@ -1,28 +1,12 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text>Another piece of text</Text>
-      </View>
-      <Text style={styles.dumyText}>Hello World!</Text>
-      <Button title="Tap Me" />
+    <View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  dumyText: {
-    margin: 16,
-    padding: 16,
-    borderColor: "blue",
-    borderWidth: 2,
-  },
+  
 });
