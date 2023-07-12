dc4328f1988b2199e25ce4185ee2acc79973032d
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-12 16:13:27 -0700

    Adding inline style to the second text box

--- App.js
+++ App.js
@@ -6,7 +6,11 @@ export default function App() {
       <View>
         <Text>Another piece of text</Text>
       </View>
-      <Text>Hello World!</Text>
+      <Text
+        style={{ margin: 16, borderColor: "red", borderWidth: 2, padding: 16 }}
+      >
+        Hello World!
+      </Text>
       <Button title="Tap Me" />
     </View>
   );
