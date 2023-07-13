8ae0cac40635805a88a6195b232aefae7bef934d
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-12 18:06:12 -0700

    Lesson 24 - Adding Rounded Corners (Device differences)
    Because `borderRadius` can't be applied to text elements on iOS, it has to be on the View, and because `color` doesn't cascade to children, it has to be added to the text component separately.

--- App.js
+++ App.js
@@ -27,7 +27,11 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+        {courseGoals.map((goal) => (
+          <View style={styles.goalItem} key={goal}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -57,5 +61,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    color: "white",
   },
 });
