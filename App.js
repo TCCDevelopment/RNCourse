254b9158be84b795ceb9e8203589c4b7a765b618
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-12 17:39:34 -0700

    Lesson 23 - Updating Goals List
    This creates a state to manage the goals list and then updates that state whenever the button is pressed that calls the 'addGoalHandler' function. It then dynamically updates the list by going through the array by mapping the various goals and then outputting the component to display the individual goal.

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
   }
 
   function addGoalHandler() {
-    console.log(enteredGoalText);
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
   }
 
   return (
@@ -23,7 +27,7 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals....</Text>
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
