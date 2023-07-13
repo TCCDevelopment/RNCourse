e3414e122ed9068f480b31f824c9f7ee15f31fcb
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-12 17:22:24 -0700

    Update App.js

--- App.js
+++ App.js
@@ -16,27 +16,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    flex:1,
+    flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
   },
-    inputContainer: {
-      flex: 1,
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      marginBottom: 24,
-      borderBottomWidth: 1,
-      borderBottomColor: "#cccccc",
-    },
-      textInput: {
-        borderWidth: 1,
-        borderColor: "#cccccc",
-        width: "70%",
-        marginRight: 8,
-        padding: 8,
-      },
-    goalsContainer: {
-      flex: 5,
-    }
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
 });
