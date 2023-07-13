d3e926c6d367f6a34078542c62da0ec7d71ee7b2
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-13 14:55:47 -0700

    Lesson 27 - Splitting the GoalItem to its own file

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import {
-  StyleSheet,
   View,
+  StyleSheet,
   FlatList,
-  Text,
   TextInput,
   Button,
 } from "react-native";
+
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -38,11 +39,7 @@ export default function App() {
           alwaysBounceVertical={false}
           data={courseGoals}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem />;
           }}
           keyExtractor={(item, index) =>{
             return item.id;
@@ -77,14 +74,5 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-  },
-  goalText: {
-    color: "white",
   },
 });
