a354d62f8ead67a17704c658a77e4a8e648df2d3
TCCDevelopment <webmaster@thechristianchain.org>
2023-07-13 17:21:01 -0700

    Lesson 28 - Moving Goal Input to a Component
    I followed along and thought that I got everything correct, but it is erroring stating `Error: Text strings must be rendered within a <Text> component.`
    
    Unfortunately the error code doesn't tell me specifically where the error is happening. So, I am going to commit these changes, then copy over the code the teacher has in the repository to learn where I went wrong to get this error.

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import { useState } from "react";
-import {
-  View,
-  StyleSheet,
-  FlatList,
-  TextInput,
-  Button,
-} from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 
-import GoalItem from './components/GoalItem';
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
-  function goalInputHandler(enteredText) {
-    setEnteredGoalText(enteredText);
-  }
-
-  function addGoalHandler() {
+  function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
       { text: enteredGoalText, id: Math.random().toString() },
@@ -26,13 +16,8 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Your course goal!"
-          onChangeText={goalInputHandler}
-        />
-        <Button title="Add Goal" onPress={addGoalHandler} />
+      <View>
+        return <GoalInput onAddGoal={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
         <FlatList
@@ -41,7 +26,7 @@ export default function App() {
           renderItem={(itemData) => {
             return <GoalItem text={itemData.item.text} />;
           }}
-          keyExtractor={(item, index) =>{
+          keyExtractor={(item, index) => {
             return item.id;
           }}
         />
@@ -55,22 +40,6 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
-    padding: 8,
   },
   goalsContainer: {
     flex: 5,
